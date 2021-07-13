b03d9ab83a94943ca2a8d8c18aa6858464be43d0
Giovanni Cristiano <giodev84@gmail.com>
2021-07-13 23:01:51 +0200

    add Spacer and Styling in SignupScreen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -2,17 +2,41 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 
+import Spacer from "../components/Spacer";
+import { theme } from "../constants/index";
+
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
-      <Text h3>Sign Up Page</Text>
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up Page</Text>
+      </Spacer>
       <Input placeholder="Email" label="Email" />
+      <Spacer style={styles.inputSeparator} />
       <Input placeholder="Password" label="Password" />
-      <Button title="Sign Up" />
-    </>
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: theme.spacing.xxxl * 1.5,
+  },
+  inputSeparator: {
+    margin: theme.spacing.sm,
+  },
+});
 
 export default SignupScreen;
