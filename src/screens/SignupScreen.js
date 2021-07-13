6c812b77917c6dbb16a1dc26a41bffab6f803575
Giovanni Cristiano <giodev84@gmail.com>
2021-07-13 21:49:25 +0200

    replace react native components with RN Elements

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,18 +1,14 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
 
 const SignupScreen = ({ navigation }) => {
   return (
     <>
-      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-      <Button
-        title="Go to Signin"
-        onPress={() => navigation.navigate("Signin")}
-      />
-      <Button
-        title="Go to MainFlow"
-        onPress={() => navigation.navigate("mainFlow")}
-      />
+      <Text h3>Sign Up Page</Text>
+      <Input placeholder="Email" label="Email" />
+      <Input placeholder="Password" label="Password" />
+      <Button title="Sign Up" />
     </>
   );
 };
