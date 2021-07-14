f6b7589120ff946a3cd37de860f45bc0b2c3f8a5
Giovanni Cristiano <giodev84@gmail.com>
2021-07-14 12:33:33 +0200

    add createDataContext

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,23 +1,43 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, SafeAreaView, StatusBar } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 
 import Spacer from "../components/Spacer";
 import { theme } from "../constants/index";
+  
+
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Spacer>
-        <Text h3>Sign Up Page</Text>
+        <Text h3>Sign up</Text>
       </Spacer>
-      <Input placeholder="Email" label="Email" />
+      <Input
+        placeholder="Email"
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <Spacer style={styles.inputSeparator} />
-      <Input placeholder="Password" label="Password" />
+      <Input
+        placeholder="Password"
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+      />
       <Spacer>
-        <Button title="Sign Up" />
+        <Button title="Sign up" />
       </Spacer>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -32,7 +52,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: theme.spacing.xxxl * 1.5,
+    marginBottom: theme.spacing.xxxl,
+    marginTop StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`
+    // marginTop: theme.spacing.sm,
   },
   inputSeparator: {
     margin: theme.spacing.sm,
