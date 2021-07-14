ebbf932709ffbfc77bd92814009fd161b41704d3
Giovanni Cristiano <giodev84@gmail.com>
2021-07-14 21:56:50 +0200

    add sign up,in,out actions

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,30 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // Try to sign up
+    // Handle success by updating state
+    // Handle failure
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // Try to sign in
+    // Handle success by updating state
+    // Handle failure
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // Try to sign out
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signout, signup },
   { isSignedIn: false }
 );
