1d53a590fd81bede0d7370e9435473570901c4c5
Giovanni Cristiano <giodev84@gmail.com>
2021-07-15 21:53:38 +0200

    configure SigninScreen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useContext } from "react";
+import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
+import { theme } from "../constants/index";
+import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
-const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+const SigninScreen = ({ navigation }) => {
+  const { state, signin } = useContext(AuthContext);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <AuthForm
+        headerText="Sign In"
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
+        buttonText="Sign In"
+      />
+      <NavLink
+        linkText="Don't have an account? Go to Sign up"
+        onNavigate={() => navigation.navigate("Signup")}
+      />
+    </SafeAreaView>
+  );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: theme.spacing.sm,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: theme.spacing.xxxl,
+    marginTop: StatusBar.currentHeight ? StatusBar.currentHeight : null,
+  },
+});
 
 export default SigninScreen;
