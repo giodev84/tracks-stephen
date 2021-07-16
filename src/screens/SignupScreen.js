794e52963f21842dbc3a1e89ce0194ae09ad07bf
Giovanni Cristiano <giodev84@gmail.com>
2021-07-16 10:46:10 +0200

    add tryLocalSignin in SignupScreen using useEffect

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
 import { NavigationEvents } from "react-navigation";
 
@@ -8,7 +8,12 @@ import AuthForm from "../components/AuthForm";
 import NavLink from "../components/NavLink";
 
 const SignupScreen = ({ navigation }) => {
-  const { state, signup } = useContext(AuthContext);
+  const { state, signup, clearErrorMessage, tryLocalSignin } =
+    useContext(AuthContext);
+
+  useEffect(() => {
+    tryLocalSignin();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
