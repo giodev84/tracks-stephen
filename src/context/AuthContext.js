c2a7d07611fea6c2d847b6f86e05baeab0c33e63
Giovanni Cristiano <giodev84@gmail.com>
2021-07-16 10:43:03 +0200

    add tryLocalSignin, check if there's already a token in the storage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -20,6 +20,16 @@ const clearErrorMessage = (dispatch) => () => {
   dispatch({ type: "clear_error_message" });
 };
 
+const tryLocalSignin = (dispatch) => async () => {
+  const token = await AsyncStorage.getItem("@token");
+  if (token) {
+    dispatch({ type: "signin", payload: token });
+    navigate("TrackList");
+  } else {
+    navigate("loginFlow");
+  }
+};
+
 const signup =
   (dispatch) =>
   async ({ email, password }) => {
@@ -54,6 +64,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signout, signup, clearErrorMessage },
+  { signin, signout, signup, clearErrorMessage, tryLocalSignin },
   { token: null, errorMessage: "" }
 );
